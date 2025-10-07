318b925388ccfdff5cf6fe242850950b63d5ef78
ath-devloper <atharvphadtare902@gmail.com>
2025-10-07 23:04:00 +0530

    ok

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
-let num=20;
-let num2=10;
-if(num>num2){
-    console.log("wow number 1st is greater",num);
+let num=212;
+let num2=num%10;
+if(num2%2===0){
+    console.log("wow number is even",num2);
 
 }else{
-    console.log("okay so the num2 is greater: ",num2)
+    console.log("okay so the num2 is odd ",num2)
 }
