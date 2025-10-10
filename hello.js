d136f068b3c35c13b77973153e990e59d471ee4c
ath-devloper <atharvphadtare902@gmail.com>
2025-10-10 10:34:47 +0530

    ok

--- hello.js
+++ hello.js
@@ -1,12 +1,14 @@
-let sum=0;
-let num=5;
-// for(let i=1;i<=num;i++){
-//     sum=sum+i;
-// }
-// console.log(sum)
-let i=1;
-while(i<=num){
-    sum=sum+i;
-    i++;
+let Number=100;
+for(let i=1;i<=Number;i++){
+    if(i%3===0 && i%5===0){
+        console.log("FizzBuzz");
+    }
+    else if(i%5===0){
+        console.log("buzz");
+    }
+    else if(i%3===0){
+        console.log("fizz");
+    }else{
+        console.log(i);
+    }
 }
-console.log(sum)
