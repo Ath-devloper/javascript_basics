d3d155065a0ce93d023265d00fc83bbdf616a24d
ath-devloper <atharvphadtare902@gmail.com>
2025-10-11 22:42:14 +0530

    added

--- hello.js
+++ hello.js
@@ -1,14 +1,8 @@
-let Number=100;
-for(let i=1;i<=Number;i++){
-    if(i%3===0 && i%5===0){
-        console.log("FizzBuzz");
-    }
-    else if(i%5===0){
-        console.log("buzz");
-    }
-    else if(i%3===0){
-        console.log("fizz");
-    }else{
-        console.log(i);
+let Number=50;
+let sum=0;
+for(let i=0;i<=Number;i++){
+    if(i%2 != 0){
+        sum=sum+i;
     }
 }
+console.log(sum)
