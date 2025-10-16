091c8558ca6ec84fd830c7ea6064ec785a815271
ath-devloper <atharvphadtare902@gmail.com>
2025-10-16 23:02:14 +0530

    ok

--- hello.js
+++ hello.js
@@ -1,6 +1,22 @@
-function addition(num1,num2){
-    let sum=num1+num2;
-    return sum;
+
+let n=20;
+
+let isprime=true;
+
+if(n<=1){
+    console.log("no no baccha enter a valid number ");
 }
-const did=addition(10,20);
-console.log(did);
+else{
+    for(let i=2;i<=Math.sqrt(n);i++){
+    if(n%i===0){
+        isprime=false;
+        break;
+    }
+}
+if(isprime){
+    console.log("this is a prime number",n);
+
+}else{
+    console.log("not a prime number");
+}
+}
